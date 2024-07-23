37b1a4b60a9204606a567e39b5cf11fb6ef6fef8
Fabienne <fabienne_berges@yahoo.fr>
2024-07-23 16:52:05 +0200

    Fix toggle Bulles img

--- js/script.js
+++ js/script.js
@@ -6,90 +6,51 @@ $(document).ready(function () {
 	$('.not-authorized-overlay').hide();
 	$('#Bulles .photo').hover(
 		function () {
-			$(this).find('.not-authorized-overlay').fadeToggle(200);
+			$(this).find('.not-authorized-overlay').toggle(200);
 		}
 	);
 });
 
 
-
-
-// Récupérer les éléments HTML nécessaires
-/*const photos = document.querySelectorAll('.photo');
-const overlays = document.querySelectorAll('.overlay');
-
-// Fonction pour retourner l'image et afficher le texte
-function flipPhoto() {
-	// Récupérer l'overlay de l'image survolée
-	const overlay = this.querySelector('.overlay');
-	// Changer la classe pour déclencher l'animation CSS
-	overlay.classList.toggle('flip');
-}
-
-// Écouter l'événement de survol sur chaque photo
-photos.forEach(photo => photo.addEventListener('mouseover', flipPhoto));
-photos.forEach(photo => photo.addEventListener('mouseout', flipPhoto));*/
-
-
 //Permet d'aller progressivement dans une section du menu
 
 $(function () {
-
 	$(".navbar a ").on("click", function (event) {
-
 		event.preventDefault();
 		var hash = this.hash;
-
-		$('body,html').animate({ scrollTop: $(hash).offset().top }, 850, function () { window.location.hash = hash; })
-
+		$('body,html').animate({ scrollTop: $(hash).offset().top }, 850, function () {
+			window.location.hash = hash;
+		})
 	});
-
 	$("#myNavbar a").on("click", function () {
-
 		$("#myNavbar").collapse("hide");
 	});
 
 	//Formulaire de contact avec message d'erreurs
-
 	// Fonction JavaScript pour l'envoi du formulaire
-
 	$('#contact-form').submit(function (e) {
 		e.preventDefault();
 		$('.comments').empty();
 		let postdata = $('#contact-form').serialize();
-
 		$.ajax({
 			type: 'POST',
 			url: 'php/contact.php',
 			data: postdata,
 			dataType: 'json',
 			success: function (result) {
-
-
 				if (result.isSuccess) {
-
 					$("#contact-form").append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
 					$("#contact-form")[0].reset();
 				}
-
 				else {
-
 					$("#firstname + .comments").html(result.firstnameError);
-
 					$("#name + .comments").html(result.nameError);
-
 					$("#email + .comments").html(result.emailError);
-
 					$("#phone + .comments").html(result.phoneError);
-
 					$("#subject + .comments").html(result.subjectError);
-
 					$("#message + .comments").html(result.messageError);
-
 				}
 			}
-
-
 		});
 	});
 });
