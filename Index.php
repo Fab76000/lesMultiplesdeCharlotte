297f6457c61b1329fd69c998f2ecbe181ffac4e6
Fabienne <fabienne_berges@yahoo.fr>
2024-09-20 19:05:44 +0200

    utilisation de script.min.js

--- Index.php
+++ Index.php
@@ -161,7 +161,7 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" defer></script>
-	<script src="js/script.js"></script>
+	<script src="js/script.min.js"></script>
 	<div id="cookie-consent-banner">
 		<img src="images/cookie.webp" alt="Cookie" class="cookie-corner-left">
 		<h2>Nous utilisons des cookies</h2>
