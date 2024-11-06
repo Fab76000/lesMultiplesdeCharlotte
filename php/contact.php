cae4003a2ad6fa8e0f1d9b33b16d2706d83b1694
Fabienne <fabienne_berges@yahoo.fr>
2024-11-06 23:25:59 +0100

    Changement adresse emailTo

--- php/contact.php
+++ php/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 $array = array("firstname" => "", "name" => "", "email" => "", "phone" => "", "subject" => "", "message" => "", "data-consent" => "",  "firstnameError" => "", "nameError" => "", "emailError" => "", "phoneError" => "", "subjectError" => "", "messageError" => "", "data-consentError" => "",  "isSuccess" => false, "cookiesSet" => false);
-$emailTo = "fabienne@fabienneberges.com";
+$emailTo = "larchotterenard@protonmail.com";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $array["firstname"] = test_input($_POST["firstname"]);
